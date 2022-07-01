08ba5f37f53e09cfaf3ca6edd95cd2271d409f0e
juliannelicon@gmail.com <juliannelicon@gmail.com>
2022-07-01 13:52:06 -0300

    Inicia projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Contato</title>
+      </Head>
+
+      <main>
+       Contato
+      </main>
+
+
+    </div>
+  )
+}
+
+export default Home
