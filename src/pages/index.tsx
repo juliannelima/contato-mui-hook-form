768962ffdf6121ce31e69f808f8b9eb70410f123
juliannelicon@gmail.com <juliannelicon@gmail.com>
2022-07-05 08:48:03 -0300

    Adicionando hook-form e yup para validar formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,47 @@ import {
   Typography,
 } from '@mui/material';
 
-import { Send, CheckCircle } from '@mui/icons-material';
+import { Send, CheckCircle, Cancel } from '@mui/icons-material';
+
+import { useForm, Controller } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { green } from '@mui/material/colors';
+
+interface IContato {
+  nome: string;
+  email: string;
+  assunto: string;
+  mensagem: string;
+  message: string;
+}
+
+const schema = yup
+  .object({
+    nome: yup
+      .string()
+      .required('Campo obrigatório')
+      .max(70, 'No máximo 70 caracteres'),
+    email: yup
+      .string()
+      .required('Campo obrigatório')
+      .email('Preencha um e-mail válido'),
+    assunto: yup
+      .string()
+      .required('Campo obrigatório')
+      .max(70, 'No máximo 70 caracteres'),
+    mensagem: yup.string().required('Campo obrigatório'),
+  })
+  .required();
 
 export default function Home() {
+  const { control, handleSubmit } = useForm<IContato>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: IContato) => console.log(data);
+
   return (
     <>
       <Head>
@@ -41,51 +79,173 @@ export default function Home() {
             Contato
           </Typography>
 
-          <Box component="form">
-            <TextField
-              label="Nome"
-              variant="outlined"
-              margin="normal"
-              size="small"
-              color="success"
-              fullWidth
-              required
-              focused
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <CheckCircle color="success" />
-                  </InputAdornment>
-                ),
-              }}
-              value="Julianne"
+          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Controller
+              name="nome"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="Nome"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  fullWidth
+                  required
+                  sx={{
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderColor: value && value.length > 0 ? green[800] : '',
+                      borderWidth: value && value.length > 0 ? 2 : 1,
+                    },
+                    '& .MuiInputLabel-root': {
+                      color: value && value.length > 0 ? green[800] : '',
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        {value && value.length > 0 && !error && (
+                          <CheckCircle color="success" />
+                        )}
+                        {!!error && <Cancel color="error" />}
+                      </InputAdornment>
+                    ),
+                  }}
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
             />
-            <TextField
-              label="E-mail"
-              variant="outlined"
-              margin="normal"
-              size="small"
-              fullWidth
-              required
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="E-mail"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  fullWidth
+                  required
+                  sx={{
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderColor: value && value.length > 0 ? green[800] : '',
+                      borderWidth: value && value.length > 0 ? 2 : 1,
+                    },
+                    '& .MuiInputLabel-root': {
+                      color: value && value.length > 0 ? green[800] : '',
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        {value && value.length > 0 && !error && (
+                          <CheckCircle color="success" />
+                        )}
+                        {!!error && <Cancel color="error" />}
+                      </InputAdornment>
+                    ),
+                  }}
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
             />
-            <TextField
-              label="Assunto"
-              variant="outlined"
-              margin="normal"
-              size="small"
-              fullWidth
-              required
+            <Controller
+              name="assunto"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="Assunto"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  fullWidth
+                  required
+                  sx={{
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderColor: value && value.length > 0 ? green[800] : '',
+                      borderWidth: value && value.length > 0 ? 2 : 1,
+                    },
+                    '& .MuiInputLabel-root': {
+                      color: value && value.length > 0 ? green[800] : '',
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        {value && value.length > 0 && !error && (
+                          <CheckCircle color="success" />
+                        )}
+                        {!!error && <Cancel color="error" />}
+                      </InputAdornment>
+                    ),
+                  }}
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
             />
-            <TextField
-              label="Mensagem"
-              variant="outlined"
-              margin="normal"
-              size="small"
-              fullWidth
-              required
-              multiline
-              maxRows={7}
-              minRows={7}
+            <Controller
+              name="mensagem"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="Mensagem"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  fullWidth
+                  required
+                  multiline
+                  maxRows={7}
+                  minRows={7}
+                  sx={{
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderColor: value && value.length > 0 ? green[800] : '',
+                      borderWidth: value && value.length > 0 ? 2 : 1,
+                    },
+                    '& .MuiInputLabel-root': {
+                      color: value && value.length > 0 ? green[800] : '',
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        {value && value.length > 0 && !error && (
+                          <CheckCircle color="success" />
+                        )}
+                        {!!error && <Cancel color="error" />}
+                      </InputAdornment>
+                    ),
+                  }}
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
             />
             <Button
               variant="contained"
